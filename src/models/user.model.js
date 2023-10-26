ebb74919fd2204e77f264e5931daceb76d630db7
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-26 14:42:15 +0600

    add user model, generate jwt token, hash pasword, compare password

---
+++ src/models/user.model.js
@@ -0,0 +1,64 @@
+const { Schema, model } = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hash password
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  next();
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (password) {
+  const user = this;
+  return await bcrypt.compare(password, user.password);
+};
+
+// Hide user credentials
+userSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+// Generate JWT
+userSchema.methods.generateJWT = function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  return token;
+};
+
+module.exports = model("User", userSchema);
