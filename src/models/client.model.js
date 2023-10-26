f9c88542e75b7e9c1caf80d56235d5ddd4b72dc3
Jobayer98 <joobayerrahman0220@gmail.com>
2023-10-26 18:02:42 +0600

    add client model

---
+++ src/models/client.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const clientSchema = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  LastName: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
+  address: {
+    street: String,
+    city: String,
+    country: String,
+    postalCode: String,
+  },
+});
+
+module.exports = model("Client", clientSchema);
